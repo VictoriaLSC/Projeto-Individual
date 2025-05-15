40f4deb8283546f0fcb347aff1a9abbb10dadec1
Victorialsc <victoria.conceicao@sptech.school>
2025-05-15 17:20:29 -0300

    conectando a API com o site

---
+++ src/routes/usuarios.js
@@ -0,0 +1,15 @@
+var express = require("express");// chama a biblioteca express
+var router = express.Router();
+
+var usuarioController = require("../controllers/usuarioController");
+
+//Recebendo os dados do html e direcionando para a função cadastrar de usuarioController.js
+router.post("/cadastrar", function (req, res) { //post: está enviando a informação / get você pega a informação
+    usuarioController.cadastrar(req, res);
+})
+
+router.post("/autenticar", function (req, res) {
+    usuarioController.autenticar(req, res);
+});
+
+module.exports = router;
